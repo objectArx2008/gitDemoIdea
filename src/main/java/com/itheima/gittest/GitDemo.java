33744764be244ae331666465765656a105061a0f
lpf <lpf@qq.com>
2022-10-23 14:11:05 +0800

    master 2th commit

--- src/main/java/com/itheima/gittest/GitDemo.java
+++ src/main/java/com/itheima/gittest/GitDemo.java
@@ -3,6 +3,7 @@ package com.itheima.gittest;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("初次提交代码！");
+        System.out.println("master第2次提交");
 
     }
 }
